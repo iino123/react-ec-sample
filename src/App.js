fc953cac13b1d52d77a2edcc0414d22529e93038
iinokenjiro <zxcvbnm.ken@gmail.com>
2019-12-11 16:19:09 +0900

    integrate react router(test)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage.component";
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
 function App() {
-  return <HomePage />;
+  return (
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/hats" component={HatsPage} />
+    </Switch>
+  );
 }
 
 export default App;
