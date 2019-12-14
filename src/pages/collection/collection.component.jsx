6041052a09f542df8ac1336b562858ef57f429d8
iinokenjiro <zxcvbnm.ken@gmail.com>
2019-12-14 18:40:14 +0900

    style: pages/collection/collection.component

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
-// import CollectionItem from "../../components/collection-item/collection-item.component";
+import CollectionItem from "../../components/collection-item/collection-item.component";
 
-import "./collection.component";
+import "./collection.styles.scss";
 import { selectCollection } from "../../redux/shop/shop.selector";
 
 const CollectionPage = ({ collection }) => {
-  console.log(collection);
-  debugger;
+  const { title, items } = collection;
   return (
-    <div className="category">
-      <h2>CATEGORY PAGE</h2>
-      {
-        // collection.items.map(item => (
-        //   <CollectionItem item={item} addItem={() => console.log(123)} />
-        // ))
-      }
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
