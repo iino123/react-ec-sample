939d05d550f950d9493789bf40bdd0bd19960f87
iinokenjiro <zxcvbnm.ken@gmail.com>
2019-12-14 18:58:42 +0900

    update: improve performance&readability by Data Normalization(Arrays -> Hash Tables)

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,13 +1,5 @@
 import { createSelector } from "reselect";
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jacket: 3,
-  womens: 4,
-  mens: 5
-};
-
 const selectShop = state => state.shop;
 
 export const selectShopCollections = createSelector(
@@ -17,8 +9,7 @@ export const selectShopCollections = createSelector(
 
 // 渡される引数によって返却したい結果が変化する場合の書き方 -> HOCを使う
 export const selectCollection = collectionUrlParam =>
-  createSelector([selectShopCollections], collections =>
-    collections.find(
-      collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [selectShopCollections],
+    collections => collections[collectionUrlParam]
   );
